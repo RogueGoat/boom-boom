2d4a84fdd73d348875261cc12dbb4f5af8088a10
Samantha <roguegoatstudios@outlook.com>
2017-07-10 20:30:40 -0500

    setup for checkpoint 1 complete

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+(function() {
+  function config($locationProvider, $stateProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+             });
+
+        $stateProvider
+            .state('list', {
+                url: '/',
+                controller: 'ListCtrl as list',
+                templateUrl: '/templates/list.html'
+            });
+    }
+     angular
+      .module('BoomBoom', ['ui.router', 'firebase'])
+ })();
---
+++ app/scripts/controllers/ListCtrl.js
@@ -0,0 +1,9 @@
+(function() {
+   function ListCtrl($scope, $uibModal, List, $cookies) {
+
+   }
+
+   angular
+     .module('BoomBoom')
+     .controller('ListCtrl', ['$scope', '$uibModal', 'List', '$cookies', ListCtrl]);
+ })();
