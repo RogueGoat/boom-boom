3aba25de84151e94cc7358b56fefac2ca67545d0
Florence <roguegoatstudios@outlook.com>
2017-08-07 06:51:41 -0500

    ryan

--- app/scripts/services/List.js
+++ app/scripts/services/List.js
@@ -3,13 +3,22 @@
       var ref = firebase.database().ref().child("lists");
       var listOfLists = $firebaseArray(ref);
 
-
       return {
          all: listOfLists,
          makeList: function(newList){
-         return listOfLists.$add(newList);
-         }
+           create_list(newList, 'unknown');
+         },
+         create_list: create_list,
      };
+
+     function create_list(title, type) {
+       listOfLists.$add({
+         title: title,
+         type: type,
+         created_at: (new Date()).getTime(),
+         updated_at: (new Date()).getTime(),
+       });
+     }
   }
 
   angular
